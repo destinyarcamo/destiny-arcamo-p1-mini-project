faebd4c7fb620d9b0e4b39e93da1a8fd29aafa81
destinyarcamo <deba.arcamo.au@phinmaed.com>
2026-08-20 12:52:36 +0800

    Create sum_numbers.py

---
+++ sum_numbers.py
@@ -0,0 +1,19 @@
+print("=" * 45)
+print("        SUM OF THREE NUMBERS")
+print("=" * 45)
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+total = num1 + num2 + num3
+
+print()
+print("-" * 45)
+print("              RESULT")
+print("-" * 45)
+print("First number:", num1)
+print("Second number:", num2)
+print("Third number:", num3)
+print("Total:", total)
+print("=" * 45)
