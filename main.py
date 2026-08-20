c16c5124ff8bf9d4f24027240afaf5628caeb793
destinyarcamo <deba.arcamo.au@phinmaed.com>
2026-08-20 12:33:57 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+number_one = float(input("Enter the first number: "))
+number_two = float(input("Enter the second number: "))
+number_three = float(input("Enter the third number: "))
+
+sum_of_numbers = number_one + number_two + number_three
+
+print("The total of the three numbers is:", sum_of_numbers)
